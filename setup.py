f197e03ff8e58a913f5bae3ca4d55930315e1b9d
phzwart <PHZwart@lbl.gov>
2023-01-25 18:32:13 -0800

    stub

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dlsia',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='Peter Zwart',
+    author_email='phzwart@lbl.gov',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
